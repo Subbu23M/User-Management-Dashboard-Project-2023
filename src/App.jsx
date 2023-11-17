02146a6ffbb9d5b773fd053284d508eb246a78ed
subramanyam <subbuvenkat2495@gmail.com>
2023-11-17 20:11:11 +0530

    initial-setup-completed

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+    return (
+        <>
+            <h1 className="text-center text-3xl capitalize my-2">
+                user management dashboard
+            </h1>
+        </>
+    )
+}
